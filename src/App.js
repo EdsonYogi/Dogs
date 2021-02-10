c2904d33dfed627eb73f3201d67fb57ecf2f6c56
Edson Yogi <edson.yogi@hotmail.com>
2021-02-10 00:10:24 -0300

    add comando deploy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <UserStorage>
           <Header />
           <Routes />
